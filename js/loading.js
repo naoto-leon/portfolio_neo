648b649feafa9c84d0ed6848d9fdca54029b0567
naoto-leon <naoto.leon@gmail.com>
2019-05-23 00:35:08 +0900

    Initial commit

---
+++ js/loading.js
@@ -0,0 +1,24 @@
+// JavaScript Document
+
+// ローディング画面をフェードインさせてページ遷移
+//$(function(){
+//    // リンクをクリックしたときの処理。外部リンクやページ内移動のスクロールリンクなどではフェードアウトさせたくないので少し条件を加えてる。
+//    $('a[href ^= "top.php"]' + 'a[target != "_blank"]').click(function(){
+//        var url = $(this).attr('href'); // クリックされたリンクのURLを取得
+//        $('#js-loader').fadeIn(600);    // ローディング画面をフェードイン
+//        setTimeout(function(){ location.href = url; }, 800); // URLにリンクする
+//        return false;
+//    });
+//});
+ 
+// ページのロードが終わった後の処理
+$(window).load(function(){
+  $('#js-loader').delay(300).fadeOut(300); //ローディング画面をフェードアウトさせることでメインコンテンツを表示
+});
+ 
+// ページのロードが終わらなくても10秒たったら強制的に処理を実行
+$(function(){ setTimeout('stopload()', 10000); });
+function stopload(){
+  $('#js-loader').delay(300).fadeOut(400); //ローディング画面をフェードアウトさせることでメインコンテンツを表示
+}
+
